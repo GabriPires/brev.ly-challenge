61d2c1ecdd5f3f46754a2e9bdf51508d1bece841
GabriPires <gabpiresdev@gmail.com>
2025-09-20 09:44:05 -0300

    test: add unit tests for custom short URL handling in createLink function

--- server/src/app/functions/create-link.spec.ts
+++ server/src/app/functions/create-link.spec.ts
@@ -2,6 +2,7 @@ import { isLeft, isRight, unwrapEither } from '@/shared/either'
 import { describe, expect, it } from 'vitest'
 import { createLink } from './create-link'
 import { InvalidUrl } from './errors/invalid-url'
+import { ResourceAlreadyExists } from './errors/resource-already-exists'
 
 describe('create link', () => {
   it('should be able to create a link', async () => {
@@ -22,5 +23,32 @@ describe('create link', () => {
 
     expect(isLeft(sut)).toBe(true)
     expect(unwrapEither(sut)).toBeInstanceOf(InvalidUrl)
+  })
+
+  it('should be able to create a link with custom short url', async () => {
+    const sut = await createLink({
+      originalUrl: 'https://example.com',
+      shortUrl: 'custom-short-url',
+    })
+
+    expect(isRight(sut)).toBe(true)
+    expect(unwrapEither(sut)).toMatchObject({
+      shortHash: 'custom-short-url',
+    })
+  })
+
+  it('should not be able to create a link with existing custom short url', async () => {
+    await createLink({
+      originalUrl: 'https://example.com',
+      shortUrl: 'custom-short-url',
+    })
+
+    const sut = await createLink({
+      originalUrl: 'https://example.com',
+      shortUrl: 'custom-short-url',
+    })
+
+    expect(isLeft(sut)).toBe(true)
+    expect(unwrapEither(sut)).toBeInstanceOf(ResourceAlreadyExists)
   })
 })
